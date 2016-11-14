fbf50a26684f0864839821123f1fc575d8579cc3
Isis van der Plas <isisvanderplas@hotmail.com>
2016-11-14 04:59:02 +0100

    template finished, but not working yet, needs some debugging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
       pug = require('pug');
       morgan = require('morgan');
+      bodyParser = require('body-parser'),
       Sequelize = require('sequelize');
 
 var app = express()
@@ -10,3 +11,55 @@ var app = express()
 app.use(morgan('dev'));
 
 app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var Message = sequelize.define('message', {
+  title: Sequelize.STRING,
+  body: Sequelize.TEXT
+});
+
+
+app.get('/', (request, response) => {
+  Message.findAll({ order: 'id ASC' }).then((messages) => {
+    response.render('messages/index', { messages: messages });
+  });
+});
+
+
+app.get('/new', (request, response) => {
+  response.render('messages/new');
+});
+
+app.post('/messages', (request, response) => {
+  console.log('message posted');
+  if (request.body.title) {
+    Message.create(request.body).then(() => {
+      response.redirect('/');
+    });
+  } else {
+    response.redirect('/new');
+  }
+});
+
+
+sequelize.sync().then(() => {
+  console.log('connected to database');
+  app.listen(3000, () => {
+    console.log('Server is now running on port 3000');
+  });
+});
+
+
+//
+// app.get('/', (req, res) => {
+//   res.redirect('/edit');
+// });
+
+
+
+// app.listen(3000, () => {
+//   console.log('Webserver is running on port 3000')
+// });
